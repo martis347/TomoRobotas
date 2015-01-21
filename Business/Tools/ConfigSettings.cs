b2fb9b4fa4c72104d6ccd7ddd924cf96c4bbf7b6
Martynas Kan <martis347@gmail.com>
2015-01-21 16:01:59 +0200

    Added ConfigSettings

---
+++ Business/Tools/ConfigSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Business.Tools
+{
+    class ConfigSettings
+    {
+        public static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                var result = appSettings[key] ?? "Not Found";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            return null;
+        }
+    }
+}
